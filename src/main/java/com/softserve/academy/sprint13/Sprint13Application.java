d8a8051074b778999e4232dae2c5b06ad4138b81
Pr0glamer <nickolayladenko89@gmail.com>
2020-07-23 22:17:38 +0300

    properties 4 postgres

--- src/main/java/com/softserve/academy/sprint13/Sprint13Application.java
+++ src/main/java/com/softserve/academy/sprint13/Sprint13Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Sprint13Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Sprint13Application.class, args);
     }
 
