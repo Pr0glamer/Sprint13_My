2b7aba2cf7cd1e9a7770059b4fc06f4b86da7c6a
Andrii <simanoav@gmail.com>
2020-07-23 21:44:32 +0300

    added .gitignore

---
+++ src/main/java/com/softserve/academy/sprint13/Sprint13Application.java
@@ -0,0 +1,13 @@
+package com.softserve.academy.sprint13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sprint13Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/softserve/academy/sprint13/Sprint13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.academy.sprint13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
