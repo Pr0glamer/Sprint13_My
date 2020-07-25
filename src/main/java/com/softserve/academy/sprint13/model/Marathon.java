667b637e5aa37553e7ce94447225d4e189bc1f2a
Pr0glamer <nickolayladenko89@gmail.com>
2020-07-25 19:22:52 +0300

    Marathon service & User service

--- src/main/java/com/softserve/academy/sprint13/model/Marathon.java
+++ src/main/java/com/softserve/academy/sprint13/model/Marathon.java
@@ -11,10 +11,42 @@ import java.util.List;
 public class Marathon {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @NotBlank
     private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<Sprint> getSprints() {
+        return sprints;
+    }
+
+    public void setSprints(List<Sprint> sprints) {
+        this.sprints = sprints;
+    }
 
     @ManyToMany
     @JoinTable(
---
+++ src/main/java/com/softserve/academy/sprint13/service/MatathonServiceImpl.java
@@ -0,0 +1,47 @@
+package com.softserve.academy.sprint13.service;
+
+import com.softserve.academy.sprint13.model.Marathon;
+import com.softserve.academy.sprint13.model.User;
+import com.softserve.academy.sprint13.repository.MarathonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MatathonServiceImpl {
+
+    @Autowired
+    private MarathonRepository marathonRepository;
+
+    public List<Marathon> getAll() {
+        return marathonRepository.getAll();
+    }
+    public Marathon getMarathonById(Long id) {
+        return marathonRepository.getMarathonById(id);
+    }
+    public Marathon createOrUpdate(Marathon entity) {
+        if(entity.getId() != null) {
+            Marathon marathon = marathonRepository.getMarathonById(entity.getId());
+
+            if(marathon!=null) {
+
+                Marathon newMar = new Marathon();
+                newMar.setTitle(entity.getTitle());
+                newMar = marathonRepository.save(newMar);
+                return newMar;
+
+            }
+
+        }
+        entity = marathonRepository.save(entity);
+        return entity;
+    }
+    public void deleteMarathonById(Long id) {
+        Marathon entity = marathonRepository.getMarathonById(id);
+        marathonRepository.deleteMarathonById(id);
+    }
+
+
+
+
+}
